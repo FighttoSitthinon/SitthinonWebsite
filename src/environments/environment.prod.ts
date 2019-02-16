636b27b0b43988e5cf8dd2720d9044c36d124f49
Sitthinon Chanaritthichai <sitthinon.c@hotmail.com>
2019-02-16 15:01:26 +0700

    update project.component and add icon website

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyAHJvlTqE-K7RVGrySv1CbHz8FxhX6Dnfk',
+    authDomain: 'sitthinonwebsite.firebaseapp.com',
+    databaseURL: 'https://sitthinonwebsite.firebaseio.com',
+    projectId: 'sitthinonwebsite',
+    storageBucket: 'sitthinonwebsite.appspot.com',
+    messagingSenderId: '1097465210869'
+  }
 };
